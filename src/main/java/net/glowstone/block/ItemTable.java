36a8b61c7be6fd25b337a563abd9a6754dfee547
Tonodus <tonodus@freenet.de>
2014-09-10 19:16:05 +0200

    Added item FlintAndSteel

--- src/main/java/net/glowstone/block/ItemTable.java
+++ src/main/java/net/glowstone/block/ItemTable.java
@@ -133,7 +133,8 @@ public final class ItemTable {
         reg(Material.STATIONARY_LAVA, new BlockLava());
         reg(Material.STANDING_BANNER, new BlockBanner());
         reg(Material.WALL_BANNER, new BlockBanner());
-
+        
+        reg(Material.FLINT_AND_STEEL, new ItemFlintAndSteel());
         reg(Material.SIGN, new ItemSign());
         reg(Material.REDSTONE, new ItemPlaceAs(Material.REDSTONE_WIRE));
         reg(Material.SUGAR_CANE, new ItemPlaceAs(Material.SUGAR_CANE_BLOCK));
